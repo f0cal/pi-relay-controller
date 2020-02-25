ff4fbf3dc0df45d402fc01f287c5c66c6bbef182
Jeffrey Urban <Jeffrey@JeffreyUrban.com>
2020-02-25 17:18:41 -0500

    Refactor in progress

--- relay_lib.py
+++ relay_lib.py
@@ -13,153 +13,69 @@ from __future__ import print_function
 import RPi.GPIO as GPIO
 import time
 
-# Turn off GPIO warnings
-GPIO.setwarnings(False)
-
-# Set the GPIO numbering convention to be header pin numbers
-GPIO.setmode(GPIO.BOARD)
-
-# The number of relay ports on the relay board.
-# This value should never change!
-NUM_RELAY_PORTS = 8
-RELAY_PORTS = ()
-RELAY_STATUS = NUM_RELAY_PORTS * [0]
-
 # Some relay boards have a default on state with a low pin
 ON_STATE = 0
 OFF_STATE = 1 - ON_STATE
-
 # Delay time between turning on next channels for `all_on` and `all_off` - for stability
 DELAY_TIME = 0.2
 
-def init_relay(port_list):
-    """Initialize the module
 
-    Args:
-        port_list: A list containing the relay port assignments
-    """
-    global RELAY_PORTS
-    print("\nInitializing relay")
-    # Get the relay port list from the main application
-    # assign the local variable with the value passed into init
-    RELAY_PORTS = port_list
-    print("Relay port list:", RELAY_PORTS)
-    # setup the relay ports for output
-    for relay in enumerate(RELAY_PORTS):
-        GPIO.setup(relay[1], GPIO.OUT)
-    # return true if the number of passed ports equals the number of ports
-    return len(RELAY_PORTS) == NUM_RELAY_PORTS
+class Relay:
+    def __init__(self, port):
+        GPIO.setup(port, GPIO.OUT)
+        self._port = port
+        self.status = OFF_STATE
 
+    def relay_on(self):
+        GPIO.output(self._port, ON_STATE)
+        self.status = ON_STATE
+        time.sleep(DELAY_TIME)
 
-def relay_on(relay_num):
-    """Turn the specified relay (by relay #) on.
-
-    Call this function to turn a single relay on.
-
-    Args:
-        relay_num (int): The relay number that you want turned on.
-    """
-    if isinstance(relay_num, int):
-        # do we have a valid relay number?
-        if 0 < relay_num <= NUM_RELAY_PORTS:
-            print('Turning relay', relay_num, 'ON')
-            GPIO.output(RELAY_PORTS[relay_num - 1], ON_STATE)
-            # set the status for this relay to 'on'
-            RELAY_STATUS[relay_num - 1] = ON_STATE
-        else:
-            print('Invalid relay #:', relay_num)
-    else:
-        print('Relay number must be an Integer value')
-
-
-def relay_off(relay_num):
-    """Turn the specified relay (by relay #) off.
-
-    Call this function to turn a single relay off.
-
-    Args:
-        relay_num (int): The relay number that you want turned off.
-    """
-    if isinstance(relay_num, int):
-        # do we have a valid relay number?
-        if 0 < relay_num <= NUM_RELAY_PORTS:
-            print('Turning relay', relay_num, 'OFF')
-            GPIO.output(RELAY_PORTS[relay_num - 1], OFF_STATE)
-            # set the status for this relay to 'off'
-            RELAY_STATUS[relay_num - 1] = OFF_STATE
-        else:
-            print('Invalid relay #:', relay_num)
-    else:
-        print('Relay number must be an Integer value')
-
-
-def relay_all_on(relay_ports=RELAY_PORTS):
-    """Turn all of the relays on.
-
-     Call this function to turn all of the relays on.
-     """
-    print('Turning all relays ON')
-    for i_relay, relay in enumerate(relay_ports):
-        GPIO.output(relay, ON_STATE)
-        RELAY_STATUS[i_relay] = ON_STATE
+    def relay_off(self):
+        GPIO.output(self._port, OFF_STATE)
+        self.status = OFF_STATE
         time.sleep(DELAY_TIME)
 
 
-def relay_all_off(relay_ports=RELAY_PORTS):
-    """Turn all of the relays on.
+class RelayControl:
 
-    Call this function to turn all of the relays on.
-    """
-    print('Turning all relays OFF')
-    for i_relay, relay in enumerate(relay_ports):
-        # turn the relay off
-        GPIO.output(relay, OFF_STATE)
-        # set the status for this relay to 'off'
-        RELAY_STATUS[i_relay] = OFF_STATE
-        time.sleep(DELAY_TIME)
+    def __init__(self, relay_list):
+        # Turn off GPIO warnings
+        GPIO.setwarnings(False)
 
+        # Set the GPIO numbering convention to be header pin numbers
+        GPIO.setmode(GPIO.BOARD)
 
-def relay_toggle_port(relay_num):
-    """Toggle the specified relay (on to off, or off to on).
+        self._relays = {}
 
-    Call this function to toggle the status of a specific relay.
+        # setup the relay ports for output
+        for relay in relay_list:
+            # TODO: Support named keys rather than addressing by port
+            self._relays[relay] = Relay(relay_list[relay])
 
-    Args:
-        relay_num (int): The relay number to toggle.
-    """
-    print('Toggling relay:', relay_num)
-    if relay_get_port_status(relay_num):
-        # it's on, so turn it off
-        relay_off(relay_num)
-    else:
-        # it's off, so turn it on
-        relay_on(relay_num)
+    def relay_on(self, relay_id):
+        self._relays[relay_id].relay_on()
 
+    def relay_off(self, relay_id):
+        self._relays[relay_id].relay_off()
 
-def relay_toggle_all_port(relay_num):
-    """Toggle the specified relay (on to off, or off to on).
+    def relay_all_on(self):
+        for relay_id in self._relays:
+            self._relays[relay_id].relay_on()
 
-    Call this function to toggle the status of a specific relay.
+    def relay_all_off(self):
+        for relay_id in self._relays:
+            self._relays[relay_id].relay_off()
 
-    Args:
-        relay_num (int): The relay number to toggle.
-    """
-    for i_relay, relay in enumerate(relay_ports):
-        print('Toggling relay:', relay_num)
-        if relay_get_port_status(relay_num):
-            relay_off(relay_num)
+    def relay_toggle_port(self, relay_id):
+        if self._relays[relay_id] == ON_STATE:
+            self._relays[relay_id].relay_off()
         else:
-            relay_on(relay_num)
+            self._relays[relay_id].relay_on()
 
+    def relay_toggle_all_port(self):
+        for relay_id in self._relays:
+            self.relay_toggle_port(relay_id)
 
-def relay_get_port_status(relay_num):
-    """Returns the status of the specified relay (True for on, False for off)
-
-    Call this function to retrieve the status of a specific relay.
-
-    Args:
-        relay_num (int): The relay number to query.
-    """
-    # determines whether the specified port is ON/OFF
-    print('Checking status of relay', relay_num)
-    return RELAY_STATUS[relay_num - 1] == ON_STATE
+    def relay_get_port_status(self, relay_id):
+        return self._relays[relay_id] == ON_STATE
--- server.py
+++ server.py
@@ -17,19 +17,14 @@ error_msg = '{msg:"error"}'
 success_msg = '{msg:"success"}'
 
 # Update the following list/tuple with the port numbers assigned to your relay board
-PORTS = (3, 5, 7, 11, 12, 13, 15, 16)
-NUM_RELAY_PORTS = len(PORTS)
+PORTS = {1: 11, 2: 13, 3: 15, 4: 16, 5: 18, 6: 22, 7: 36, 8: 37}
+NUM_RELAY_PORTS = len(PORTS.keys())
 
 RELAY_NAME = 'AstroBox Relay Controller'
 
 # initialize the relay library with the system's port configuration
-if init_relay(PORTS):
-    # turn all of the relays off, so we're starting with a clean slate.
-    relay_all_off()
-else:
-    print("Port configuration error")
-    # exit the application
-    sys.exit(0)
+relay = RelayControl(PORTS)
+relay.relay_all_off()
 
 app = Flask(__name__)
 
@@ -39,16 +34,6 @@ root_dir = '/home/pi/pi-relay-controller'
 with open('{}/channels.json'.format(root_dir)) as json_file:
     channel_config = json.load(json_file)
 
-supported_channels = []
-for channel in channel_config['channels']:
-    if channel['active'] == 'true':
-        print('channel: ', channel['channel'])
-        supported_channels.append(PORTS[channel['channel'] - 1])
-    else:
-        relay_off(channel['channel'])
-
-print(supported_channels)
-
 @app.route('/')
 def index():
     print("Loading app Main page")
@@ -57,7 +42,7 @@ def index():
 
 @app.route('/status/<int:relay>')
 def api_get_status(relay):
-    res = relay_get_port_status(relay)
+    res = relay.relay_get_port_status(relay)
     if res:
         print("Relay is ON")
         return make_response("1", 200)
@@ -69,7 +54,7 @@ def api_get_status(relay):
 @app.route('/toggle/<int:relay>')
 def api_toggle_relay(relay):
     print("Executing api_relay_toggle:", relay)
-    relay_toggle_port(relay)
+    relay.relay_toggle_port(relay)
     return make_response(success_msg, 200)
 
 
@@ -78,7 +63,7 @@ def api_relay_on(relay):
     print("Executing api_relay_on:", relay)
     if validate_relay(relay):
         print("valid relay")
-        relay_on(relay)
+        relay.relay_on(relay)
         return make_response(success_msg, 200)
     else:
         print("invalid relay")
@@ -90,7 +75,7 @@ def api_relay_off(relay):
     print("Executing api_relay_off:", relay)
     if validate_relay(relay):
         print("valid relay")
-        relay_off(relay)
+        relay.relay_off(relay)
         return make_response(success_msg, 200)
     else:
         print("invalid relay")
@@ -100,14 +85,14 @@ def api_relay_off(relay):
 @app.route('/all_on/')
 def api_relay_all_on():
     print("Executing api_relay_all_on")
-    relay_all_on(supported_channels)
+    relay.relay_all_on()
     return make_response(success_msg, 200)
 
 
 @app.route('/all_off/')
 def api_all_relay_off():
     print("Executing api_relay_all_off")
-    relay_all_off(supported_channels)
+    relay.relay_all_off()
     return make_response(success_msg, 200)
 
 @app.route('/reboot/<int:relay>')
@@ -115,9 +100,9 @@ def api_relay_reboot(relay, sleep_time=3):
     print("Executing api_relay_reboot:", relay)
     if validate_relay(relay):
         print("valid relay")
-        relay_off(relay)
+        relay.relay_off(relay)
         time.sleep(sleep_time)
-        relay_on(relay)
+        relay.relay_on(relay)
         return make_response(success_msg, 200)
     else:
         print("invalid relay")
@@ -145,4 +130,3 @@ if __name__ == "__main__":
     # On the Pi, you need to run the app using this command to make sure it
     # listens for requests outside of the device.
     app.run(host='0.0.0.0', port=80)
-    #app.run(host='0.0.0.0', port=5000, debug=True)
